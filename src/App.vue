d3148927a23772104eaa4e3ec61d116a14c11498
liutao <947809647@qq.com>
2016-09-08 10:52:46 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    123123
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+@import './assets/css/index';
+@import './assets/css/ionicicon';
+html, body {
+  box-sizing: border-box;
+  background: #f5f5f5;
+}
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
